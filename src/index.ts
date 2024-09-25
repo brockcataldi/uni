6f72bab729e7f3605172e7d3519731a2f082507d
Brock <brock@Brocks-Macbook-Pro.local>
2024-09-24 23:20:47 -0400

    Fixed the swagger instance

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import express, { Express } from "express";
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from "swagger-jsdoc";
 
 import { router as studentsRouter} from './routes/students';
 
 const app: Express = express();
 const port = 3000;
 
+const options = {
+  failOnErrors: true,
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "University Enrollment System",
+      version: "0.0.0",
+      description:
+        "Job Interview Assignment",
+    },
+  },
+  apis: ["**/*.ts"],
+};
+
+const specs = swaggerJSDoc(options);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs))
 app.use("/students", studentsRouter);
+
 
 app.listen(port, function(){
   console.log(`[server]: Server is running at http://localhost:${port}`);
--- src/routes/students.ts
+++ src/routes/students.ts
@@ -1,3 +1,28 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Student:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *         - finished
+ *       properties:
+ *         student_id:
+ *           type: string
+ *           description: The auto-generated id of the student
+ *         name:
+ *           type: string
+ *           description: The name of the Student
+ *         email:
+ *           type: string
+ *           description: The Student's email
+ *       example:
+ *         id: 1
+ *         name: Example Exampleton
+ *         email: example@example.com
+ */
 import { Router, Request, Response } from "express";
 import { query } from "../database";
 
